2d20373becd0d7be87116fd3d478c9fef95c379e
Vivekrathore13 <148472573+Vivekrathore13@users.noreply.github.com>
2023-10-23 09:02:12 -0400

    selection sort

---
+++ selectionsort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n ;
+    cout<<" enter the size of array ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Shailendra yadav : 0832CS221184"<<endl;
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+
+        //selection sort
+        for(int i=0;i<n-1;i++){
+            int min =INT_MAX;
+            int mindx = -1;
+            //minimum calculation for selecting the number
+            for(int j=i;j<n;j++){
+                if (arr[j]<min){
+                    min = arr[j];
+                    mindx =j;
+                }
+            }
+            swap(arr[i],arr[mindx]);
+        }
+         for(int i=0;i<n;i++){//n-1 passes
+        cout<<arr[i]<<" ";
+        }       
+}
